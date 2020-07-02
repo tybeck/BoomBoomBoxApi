2fe7350b16081aafbc2eb82952946403bf318ed6
Tyler Beck <tbeck@topofmind.com>
2020-07-01 21:03:51 -0400

    Controller and Service

--- src/service/app.service.ts
+++ src/service/app.service.ts
@@ -8,6 +8,15 @@ export class AppService {
   constructor() {}
 
   public async beginSequence() {
-    Pin.writeSync(0);
+    Pin.writeSync(1);
+    return new Promise(resolve => {
+      setTimeout(() => {
+        Pin.writeSync(0);
+        setTimeout(() => {
+          Pin.writeSync(1);
+          resolve();
+        }, 1500);
+      }, 100);
+    });
   }
 }
